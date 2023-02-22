746d2417c2c7cec87b0fa33ace6c50f8fd6be86e
sriram-testing <srirambtechece88@gmail.com>
2023-02-23 02:14:39 +0530

    1st Commit

---
+++ program1.py
@@ -0,0 +1,15 @@
+print("Welcome to Python")
+
+
+def printer():
+    print("My First Definition call")
+
+
+printer()
+
+
+def printer1(a):
+    print("My First Parameterized Definition call"+a)
+
+
+printer1("Sairam")
